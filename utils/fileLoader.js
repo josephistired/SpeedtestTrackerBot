2b51d6ed6184e43547eef8cc404e014db589e15b
Joseph Carmosino <jc@josephcarmosino.com>
2024-06-30 03:03:29 -0400

    change to fs - linux issues, change service file.

--- utils/fileLoader.js
+++ utils/fileLoader.js
@@ -1,14 +1,40 @@
-const { glob } = require("glob");
+GNU nano 5.4                                    fileLoader.js                                              
+const fs = require('fs');
+const path = require('path');
 
 async function loadFiles(dirName) {
-  const files = await glob(
-    `${process.cwd().replace(/\\/g, "/")}/${dirName}/**/*.js`,
-    { promise: true }
-  );
+  console.log("Running loadFiles");
 
-  files.forEach((file) => delete require.cache[require.resolve(file)]);
+  const directoryPath = path.join(process.cwd(), dirName.toLowerCase());
+  // console.log(`Directory Path: ${directoryPath}`);
 
-  return files;
+  try {
+    const files = await getFiles(directoryPath);
+    files.forEach((file) => delete require.cache[require.resolve(file)]);
+    // console.log(`Files found: ${files.length}`, files);
+    return files;
+  } catch (err) {
+    // console.error(`Error loading files from ${dirName}:`, err);
+    return [];
+  }
+}
+
+function getFiles(dir, files_ = []) {
+  try {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+      const name = path.join(dir, file);
+      if (fs.statSync(name).isDirectory()) {
+        getFiles(name, files_);
+      } else if (file.endsWith('.js')) {
+        files_.push(name);
+      }
+    }
+  } catch (err) {
+    console.error(`Error reading directory ${dir}:`, err);
+  }
+  return files_;
 }
 
 module.exports = { loadFiles };
+
